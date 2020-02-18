6142ce3117e68a70e2640f9e36f88030c55801d1
cait <cait@bairuitech.com>
2020-02-18 19:47:25 +0800

    项目初始化
    Change-Id: I8ea12d9aaaae51ca06c0e819757f8ffc8f67be06

---
+++ guli-microservice-edu/src/main/java/com/online/edu/eduservice/controller/EduTeacherController.java
@@ -0,0 +1,21 @@
+package com.online.edu.eduservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author caitao
+ * @since 2020-02-18
+ */
+@RestController
+@RequestMapping("/eduservice/edu-teacher")
+public class EduTeacherController {
+
+}
+
---
+++ guli-microservice-edu/src/main/java/com/online/edu/eduservice/service/EduTeacherService.java
@@ -0,0 +1,16 @@
+package com.online.edu.eduservice.service;
+
+import com.online.edu.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 讲师 服务类
+ * </p>
+ *
+ * @author caitao
+ * @since 2020-02-18
+ */
+public interface EduTeacherService extends IService<EduTeacher> {
+
+}
